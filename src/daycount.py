cb3f636f7898a56225a60a2bdb5c103f82b84dcf
Rob Granger <rgranger@actualizeconsulting.com>
2015-10-04 12:58:05 -0400

    working again

--- src/daycount.py
+++ src/daycount.py
@@ -42,24 +42,24 @@ class MainPage(webapp.RequestHandler):
         if user:
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
-# GQL is similar to SQL             
-            daycountquery = DayCountModel.gql("WHERE author = :author",
-                                                author=users.get_current_user())
-            if daycountquery.count(1) > 0:
-                daycount = daycountquery.get()
-            else:
-                daycount = DayCountModel(author = users.get_current_user(),
-                                         basis = "Select Basis",
-                                         inclusionoption = "Select Option")    
-                daycount.put();
+      
+        daycountquery = DayCountModel.gql("WHERE author = :author",
+                                          author=users.get_current_user())
+        if daycountquery.count(1) > 0:
+            daycount = daycountquery.get()
+        else:
+            daycount = DayCountModel(author = users.get_current_user(),
+                                     basis = "Select Basis",
+                                     inclusionoption = "Select Option")    
+            daycount.put();
         
-            values = {
-                      'daycount': daycount,
-                      'user': user,
-                      'url': url,
-                      'url_linktext': url_linktext,
-                      }
-            self.response.out.write(template.render('main.html', values))
+        values = {
+                 'daycount': daycount,
+                 'user': user,
+                 'url': url,
+                 'url_linktext': url_linktext,
+                 }
+        self.response.out.write(template.render('main.html', values))
 
 # This class calculates and saves Day Count results
 class Calculate(webapp.RequestHandler):
@@ -68,21 +68,24 @@ class Calculate(webapp.RequestHandler):
         if user:
             daycountquery = DayCountModel.gql("WHERE author = :author",
                                                author=users.get_current_user())
-            if daycountquery.count(1) > 0:
-                startDate = datetime.strptime(self.request.get('startDate'), "%Y-%m-%d").date()
-                endDate = datetime.strptime(self.request.get('endDate'), "%Y-%m-%d").date()
-                
+            if daycountquery.count(1) > 0:              
                 daycount = daycountquery.get()
                 daycount.basis = self.request.get('dayBasis')
                 daycount.inclusionoption = self.request.get('inclusionOption')
-                daycount.startdate = startDate
-                daycount.enddate = endDate
+                daycount.startdate = self.setDate(self.request.get('startDate'))
+                daycount.enddate = self.setDate(self.request.get('endDate'))
                 daycount.result = daycount.calcResult()
             
                 daycount.put();
                 
             self.redirect('/')  
             
+    def setDate(self, dateString):
+        try:
+            return datetime.strptime(dateString, "%Y-%m-%d").date()
+        except ValueError:
+            return datetime.strptime('2015-01-01', "%Y-%m-%d").date()
+                
 # Register the URL with the responsible classes
 application = webapp.WSGIApplication([('/', MainPage),
                                       ('/calculate', Calculate)],
@@ -90,7 +93,7 @@ application = webapp.WSGIApplication([('/', MainPage),
 
 # Register the wsgi application to run
 def main():
-  run_wsgi_app(application)
+    run_wsgi_app(application)
   
 if __name__ == '__main__':
     main()
